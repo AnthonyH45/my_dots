ec48b4de680826f4bdd4c9853eab3ac73dcccb0f
Anthony Hallak <ahall012@ucr.edu>
2020-08-13 18:30:14 -0700

    setup

--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
 #/bin/sh
 
-printf 'This script makes a backup of existing .vimrc, .Xresources, .zshrc'
+printf 'This script makes a backup of existing .vimrc, .Xresources, .zshrc, '
 printf '\nIt then downloads the custom dotfiles I made'
 printf '\nPlease run from the home directory too!'
-printf '\nYou must have wget installed (as well as vim and zsh with oh-my-zsh)'
-# I am working on a script to download those and set them up as well
-# TODO: install wget, vim, zsh, urxvt, gdb
-# THEN: install dot files, then automate setup
-# More to come
+printf '\nYou must have wget and git installed, this script also install vim, zsh, zsh-prezto, gdb, gdb-ped'
+printf '\nThis script also assumes you have a debian system, please change the "apts" and what not\n to your distro specific installation cmds'
 printf '\nPress [ENTER] to continue . . .'
 read _
 
 clear
+
+# install vim,zsh,zsh-prezto, gdb, gdb-peda
+# vim
+sudo apt-get install vim -y
+sudo apt-get install zsh && zsh -c 'git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" '
+sudo apt-get install gdb && git clone https://github.com/longld/peda.git ~/peda && echo "source ~/peda/peda.py" >> ~/.gdbinit
 
 # check if a file exists, then back it up
 # then download the new dot file
@@ -28,6 +31,8 @@ fi
 if [ -f ~/.Xresources ]; then
     mv ~/.Xresources ~/.OLDXresources
 fi
+
+# downloads the rc files
 
 wget https://raw.githubusercontent.com/AnthonyH45/my_dots/master/.vimrc
 wget https://raw.githubusercontent.com/AnthonyH45/my_dots/master/.zshrc
